69b65b375ddef8124e3c2dc36881ef4788a51ed8
Sheargrub <sheargrub@gmail.com>
2025-05-14 15:39:57 -0500

    Add dependencies to enable R kernel

--- install.R
+++ install.R
@@ -1,2 +1,6 @@
+install.packages("devtools")
+devtools::install_github("IRkernel/IRkernel")
+IRkernel::installspec()
+
 install.packages(c("recipes", "parsnip", "tune", "dials", "workflows", "yardstick", "bonsai", "tidymodels", "tidyverse", "hydroGOF", "lightgbm", "ggthemes", "glue", "tidync", "terra", "dataRetrieval", "tsibble", "zoo", "slider", "sbtools"))
 remotes::install_github('mikejohnson51/nwmTools')
