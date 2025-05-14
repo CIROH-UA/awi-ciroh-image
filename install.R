c9f204219ac0f5fed4d702d978dcc7494b60166a
Sheargrub <sheargrub@gmail.com>
2025-05-14 14:38:30 -0500

    Major refactor
    Found an alternate approach that (in theory) supports nwmTools! Fingers crossed

---
+++ install.R
@@ -0,0 +1,2 @@
+install.packages(c("recipes", "parsnip", "tune", "dials", "workflows", "yardstick", "bonsai", "tidymodels", "tidyverse", "hydroGOF", "lightgbm", "ggthemes", "glue", "tidync", "terra", "dataRetrieval", "tsibble", "zoo", "slider", "sbtools"))
+remotes::install_github('mikejohnson51/nwmTools')
